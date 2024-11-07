4f489e54d612d93cbc7dac51a1a8e66094163961
cloudswirls24 <sepiacoloured@gmail.com>
2024-11-07 09:32:44 +0000

    Add ejs files for remaining pages and add ejs code to direct to pages

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/meetingdetails", (req, res) => {
+  res.render("meetingdetails.ejs");
+});
+
+app.get("/calendar", (req, res) => {
+  res.render("calendar.ejs");
+});
+
+app.get("/email", (req, res) => {
+  res.render("email.ejs");
+});
+
+app.get("/completed", (req, res) => {
+  res.render("completed.ejs");
+});
+
 // Tells the app which port to run on
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
