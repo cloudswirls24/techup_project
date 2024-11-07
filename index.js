d648652be52dc17db537a24ea5bed8d32bfa0c66
cloudswirls24 <sepiacoloured@gmail.com>
2024-11-07 11:00:18 +0000

    Correct hyperlinks and add script for meeting details page

--- index.js
+++ index.js
@@ -30,6 +30,28 @@ app.get("/completed", (req, res) => {
   res.render("completed.ejs");
 });
 
+
+//Code for meeting details page
+const mtgArray = ["meetingTitle", "meetingDate","startTime", "endTime", "meetingVenue", "defaultSetting","daysAgenda","priorAgenda","daysClear","priorClear","daysSubmitMaterials","priorSubmitMaterials","daysCirculate","priorCirculate"]
+mtgArray.forEach((element) => {
+  var element = element;
+  console.log(element);
+});
+
+function meetingDetails(req, res, next) {
+  mtgArray.forEach((element) => {
+    element = req.body["element"];
+    console.log(element);
+});
+  next();
+}
+
+
+app.post("/submitdetails", (req, res) => {
+  app.use(meetingDetails);
+  res.render("calendar.ejs");
+  });
+
 // Tells the app which port to run on
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
