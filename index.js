45e3623cde3656a9c43620140f02f07e66d56f40
cloudswirls24 <sepiacoloured@gmail.com>
2024-11-07 09:10:40 +0000

    add port number to index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-app.use(express.static("public"));
+const port = 10000;
+
+app.use(express.static(__dirname + "public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+// Tells the app which port to run on
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
