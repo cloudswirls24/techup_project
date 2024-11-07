07230d5dab99bb55101b56a91192616fb07bfb76
cloudswirls24 <sepiacoloured@gmail.com>
2024-11-07 15:35:39 +0000

    debug16

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 10000;
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
